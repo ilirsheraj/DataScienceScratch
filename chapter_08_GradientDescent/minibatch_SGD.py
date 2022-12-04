54e3d1a98ecdb507a2109859b1b0ce61a7af3368
Like <sherajilir@gmail.com>
2022-12-04 01:45:29 +0100

    Add files via upload

--- chapter_08_GradientDescent/minibatch_SGD.py
+++ chapter_08_GradientDescent/minibatch_SGD.py
@@ -3,7 +3,7 @@ from typing import TypeVar, List, Iterator
 from basis_code import distance, add, scalar_multiply, vector_mean, linear_gradient, gradient_step
 
 
-T = TypeVar("T")  # This allows us to use "generic" functions
+T = TypeVar('T')  # This allows us to use "generic" functions
 
 
 def minibatches(dataset: List[T], batch_size: int, shuffle: bool=True) -> Iterator[List[T]]:
@@ -26,6 +26,19 @@ for epoch in range(1000):
 	for batch in minibatches(inputs, batch_size=20):
 		grad = vector_mean([linear_gradient(x, y, theta) for x, y in batch])
 		theta = gradient_step(theta, grad, -learning_rate)
+	print(epoch, theta)
+
+slope, intercept = theta
+print(slope, intercept)
+
+
+# Stochastic Gradient Descent
+theta = [random.uniform(-1, 1), random.uniform(-1, 1)]
+inputs = [(x, 20 * x + 5) for x in range(-50, 50)]
+for epoch in range(100):
+	for x, y in inputs:
+		grad = linear_gradient(x, y, theta)
+		theta = gradient_step(theta, grad, - learning_rate)
 	print(epoch, theta)
 
 slope, intercept = theta
